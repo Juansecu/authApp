a1bfa6266c311fa2f5d838ff43d51aa381803638
Juansecu <juansecoder@hotmail.com>
2021-08-05 16:26:05 -0500

    Login form was setted as reactive form

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form: FormGroup = this._FORM_BUILDER.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/[a-z0-9]/i),
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ],
+    ],
+  });
 
-  constructor() { }
+  constructor(private readonly _FORM_BUILDER: FormBuilder) {}
 
-  ngOnInit(): void {
+  login(): void {
+    console.log(this.form.value);
   }
-
 }
