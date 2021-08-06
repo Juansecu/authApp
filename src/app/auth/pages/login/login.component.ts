6ae433189b403f03ec4e6bb75edd79353ef2887c
Juansecu <juansecoder@hotmail.com>
2021-08-06 08:48:40 -0500

    Error messages from server are displayed now when user get an error while trying to log in

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +11,7 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class LoginComponent {
+  errors: any[] = [];
   form: FormGroup = this._FORM_BUILDER.group({
     email: ['', [Validators.required, Validators.email]],
     password: [
@@ -23,10 +27,45 @@ export class LoginComponent {
 
   constructor(
     private readonly _FORM_BUILDER: FormBuilder,
-    private readonly _ROUTER: Router
+    private readonly _ROUTER: Router,
+    private readonly _AUTH_SERVICE: AuthService
   ) {}
 
   login(): void {
-    this._ROUTER.navigate(['/users/dashboard']);
+    const { email, password } = this.form.value;
+
+    this._AUTH_SERVICE.login(email, password).subscribe((response) => {
+      if (typeof response === 'object') {
+        if ((response as any).errors) {
+          let errorTemplate = `
+            You have the next errors:
+            <ul>
+          `;
+
+          this.errors = [];
+
+          if ((response as any).errors.email)
+            this.errors.push((response as any).errors.email);
+
+          if ((response as any).errors.password)
+            this.errors.push((response as any).errors.password);
+
+          for (const error of this.errors) {
+            errorTemplate += `
+              <li>- ${error.msg}</li>
+            `;
+          }
+
+          errorTemplate += `
+            </ul>
+          `;
+
+          Swal.fire({
+            icon: 'error',
+            html: errorTemplate,
+          });
+        } else Swal.fire('Error', (response as any).message, 'error');
+      } else this._ROUTER.navigate(['/users/dashboard']);
+    });
   }
 }
