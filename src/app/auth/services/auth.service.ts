bf2198def0e9c53504b0c201768e496fca44b412
Juansecu <juansecoder@hotmail.com>
2021-08-06 06:45:55 -0500

    Behavior of login method from AuthService were modified through some pipes from RxJS

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 
 import { AuthResponse } from '../typings/AuthResponse';
+import { User } from 'src/app/users/typings/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _user!: User;
+
   private readonly _BASE_URL = `${environment.serverUrl}/users`;
+
+  get user(): User {
+    return this._user;
+  }
 
   constructor(private readonly _HTTP: HttpClient) {}
 
@@ -21,11 +29,26 @@ export class AuthService {
     password: string
   ): Observable<AuthResponse> {}*/
 
-  login(email: string, password: string): Observable<AuthResponse> {
-    return this._HTTP.post<AuthResponse>(`${this._BASE_URL}/login`, {
-      email,
-      password,
-    });
+  login(email: string, password: string): Observable<boolean> {
+    return this._HTTP
+      .post<AuthResponse>(`${this._BASE_URL}/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          if (response.ok) {
+            this._user = {
+              userId: response.userId!,
+              email,
+              firstName: response.firstName!,
+              lastName: response.lastName!,
+            };
+          }
+        }),
+        map(({ ok }) => ok),
+        catchError((error) => of(false))
+      );
   }
 
   // renewToken(): Observable<AuthResponse> {}
